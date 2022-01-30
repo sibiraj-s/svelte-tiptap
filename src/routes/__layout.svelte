a1c10317c0de03e822206fc064d59621ef312b17
Sibiraj <dev.sibiraj@outlook.com>
2022-01-30 18:56:17 +0530

    Update demo site styles

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,7 @@
   import '../app.scss';
 </script>
 
-<main class="container mx-auto py-10 md:px-32">
+<main class="container mx-auto py-10 px-4 sm:px-16 md:px-32">
   <nav class="mb-4">
     <a href="{base}/" class="text-blue-600 hover:underline">Advanced Editor</a>
     <a href="{base}/floating-menu" class="text-blue-600 hover:underline ml-1">Floating Menu</a>

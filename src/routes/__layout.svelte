b714dbe66e997281dbd65367f1170a5a60664fe6
Sibiraj <dev.sibiraj@outlook.com>
2022-01-30 17:22:49 +0530

    Fix navigation in demo site

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,8 +5,8 @@
 <main class="container mx-auto py-10 md:px-32">
   <nav class="mb-4">
     <a href="/" class="text-blue-600 hover:underline">Advanced Editor</a>
-    <a href="/floating-menu" class="text-blue-600 hover:underline ml-1">Floating Menu</a>
-    <a href="/bubble-menu" class="text-blue-600 hover:underline ml-1">Bubble Menu</a>
+    <a href="floating-menu" class="text-blue-600 hover:underline ml-1">Floating Menu</a>
+    <a href="bubble-menu" class="text-blue-600 hover:underline ml-1">Bubble Menu</a>
   </nav>
   <slot />
 </main>

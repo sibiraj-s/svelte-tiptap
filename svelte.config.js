0c204ffd697dd4eed4c021343b19be9101dda65b
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2022-12-15 11:05:31 +0530

    Update to svelte kit v1 stable

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-static';
 
 const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: vitePreprocess(),
 
   kit: {
     // generate static site

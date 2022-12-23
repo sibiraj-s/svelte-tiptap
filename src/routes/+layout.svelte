65ecdade66925a02ccfa4fe990a5367539164a41
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2022-12-23 11:08:12 +0530

    Update demo page layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <main class="container mx-auto py-10 px-4 sm:px-16 md:px-32">
-  <nav class="mb-4">
+  <nav class="mb-4 flex gap-4">
     <a href="{base}/" class="text-blue-600 hover:underline">Advanced Editor</a>
-    <a href="{base}/floating-menu" class="text-blue-600 hover:underline ml-1">Floating Menu</a>
-    <a href="{base}/bubble-menu" class="text-blue-600 hover:underline ml-1">Bubble Menu</a>
+    <a href="{base}/floating-menu" class="text-blue-600 hover:underline">Floating Menu</a>
+    <a href="{base}/bubble-menu" class="text-blue-600 hover:underline">Bubble Menu</a>
   </nav>
   <slot />
 </main>

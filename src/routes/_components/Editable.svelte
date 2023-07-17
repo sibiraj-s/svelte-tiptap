f3f4df09526436f02b7573800269ab0d60071dff
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2023-07-17 11:09:12 +0530

    Update Editable Component example

--- src/routes/_components/Editable.svelte
+++ src/routes/_components/Editable.svelte
@@ -5,7 +5,6 @@
 <NodeViewWrapper
   id="svelte-component"
   class="bg-yellow-200/90 border-black border-2 px-4 pb-4 my-4 rounded-md flex flex-col"
-  data-drag-handle=""
 >
   <span
     class="bg-black py-1 px-3 text-white rounded-b-md uppercase text-[0.6rem] font-bold w-max"

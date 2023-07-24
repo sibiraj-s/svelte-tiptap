2daea6da31648ec321453cebc5a1422f1a5afe5d
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2023-07-24 21:59:35 +0530

    Update examples

--- src/routes/_components/Editable.svelte
+++ src/routes/_components/Editable.svelte
@@ -8,19 +8,16 @@
 
 <NodeViewWrapper
   id="svelte-component"
-  class={cx('bg-yellow-200/90 border-black border-2 px-4 pb-4 rounded-md flex flex-col my-4 cursor-default', {
+  class={cx('bg-yellow-200/90 border-black border-2 px-4 pb-4 rounded-md flex flex-col my-4', {
     'border-green-500': selected,
     selected,
   })}
   data-drag-handle=""
 >
   <span
-    class="bg-black py-1 px-3 text-white rounded-b-md uppercase text-[0.6rem] font-bold w-max"
+    class="bg-black py-1 px-3 text-white rounded-b-md uppercase text-[0.6rem] font-bold w-max cursor-default"
     contenteditable="false">Svelte Editable Component</span
   >
 
-  <NodeViewContent
-    class="mt-4 border-dashed border-gray-900/20 border-2 p-2 rounded-md cursor-text"
-    id="editable-component"
-  />
+  <NodeViewContent class="mt-4 border-dashed border-gray-900/20 border-2 p-2 rounded-md" id="editable-component" />
 </NodeViewWrapper>

8213ccef2c88b1fae5357fe0000a30071826cc45
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2023-11-08 16:11:38 +0530

    Remove reference to svelte internals in tests

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,18 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [sveltekit()],
+  // @fixme
+  // see https://github.com/testing-library/svelte-testing-library/issues/222
+  resolve: {
+    ...(process.env.VITEST
+      ? {
+          conditions: ['default', 'module', 'import', 'browser'],
+        }
+      : null),
+  },
   test: {
     environment: 'happy-dom',
     include: ['tests/**/*.{test,spec}.{js,ts}'],
     setupFiles: ['tests/setup.ts'],
-    alias: [
-      {
-        // @fixme
-        // see https://github.com/testing-library/svelte-testing-library/issues/222
-        find: /^svelte$/,
-        replacement: 'svelte/internal',
-      },
-    ],
   },
 });
